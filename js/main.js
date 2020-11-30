7faa14baf1b69f7ee62ece511887f6531cb015f7
Luan da Silva Ramalho <luanfswd@gmail.com>
2020-11-30 16:25:25 -0300

    Primeira parte das aulas

---
+++ js/main.js
@@ -0,0 +1,63 @@
+// var nome = "Luan da Silva Ramalho"
+// var idade = 29
+
+// alert(nome + " tem " + idade + " anos")
+
+// var nome = "Luan da Silva Ramalho"
+// var idade = 29
+// var idade2 = 43
+// alert(idade + idade2)
+
+// var idade = "29"
+// var idade2 = "43"
+// alert(idade + idade2)
+
+// var frase = "Barcelona é o melhor time do mundo!"
+// console.log(frase.replace("Barcelona","Flamengo"))
+// console.log(frase.toUpperCase())
+
+// var lista = ["maçã", "pêra", "laranja"]
+
+// console.log(lista[0])
+// console.log(lista.push("uva"))
+// console.log(lista)
+// console.log(lista.pop("laranja"))
+// console.log(lista)
+// console.log(lista.toString())
+// console.log(lista.join(" <> "))
+
+
+// var fruta = {nome:"maçã" , cor:"vermelha"}
+// console.log(fruta.nome)
+
+// var frutas = [{nome:"maçã" , cor:"vermelha"},{nome:"banana" , cor:"amarelo"}]
+// console.log(frutas[1].nome)
+
+// var idade = prompt("Qual sua idade?")
+
+// if(idade >= 18){
+//     alert("Maior de idade")
+// }
+// else{
+//     alert("Menor de idade")
+// }
+
+// var count = 1
+// while(count <= 5){
+//     console.log(count)
+//     alert(count)
+//     count++
+// }
+
+// var count
+// for(count = 0; count <= 5; count++){
+//     alert(count)
+// }
+
+// var d = new Date()
+// alert(d.getMonth()+1)
+// alert(d.getDay())
+// alert(d.getMinutes())
+//  alert(d.getHours())
+
+ 
