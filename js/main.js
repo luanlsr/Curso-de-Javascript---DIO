b0094d5a84b198e69e5845d3d158853dd7b78a93
Luan da Silva Ramalho <luanfswd@gmail.com>
2020-12-02 12:22:47 -0300

    Novos exercícios de JS

--- js/main.js
+++ js/main.js
@@ -60,4 +60,130 @@
 // alert(d.getMinutes())
 //  alert(d.getHours())
 
- 
+// function soma(n1,n2){
+//     return n1 + n2
+// }
+// alert(soma(5,10))
+
+// function multi(n1,n2){
+//     return n1 * n2
+// }
+// alert(multi(5,10))
+
+// function div(n1,n2){
+//     return n1 / n2
+// }
+// alert(div(10,5))
+
+// function sub(n1,n2){
+//     return n1 - n2
+// }
+// alert(sub(10,5))
+
+// function setReplace(frase, nome, novo_nome){
+//   return frase.replace(nome, novo_nome)
+// }
+// alert(setReplace("Vai Japão" , "Japão", "Brasil"))
+
+// function validaIdade(idade){
+//     var validar
+//     if(idade >= 18){
+//         validar = true
+//     }else{
+//         validar = false
+//     }
+//     return validar
+// }
+// var idade = prompt ("Qual a sua idade?")
+// console.log(validaIdade(idade))
+
+// function clicou(){
+//     alert("Obrigado por clicar!")
+// }
+
+// function somar(){
+//     var n1, n2
+//     n1 = 2
+//     n2 = 8
+//     var soma = n1 + n2
+    
+//     alert(soma)
+// }
+
+//CALCULADORA SIMPLES :)
+// function somar(){
+//     var n1, n2
+//     n1 = prompt("Digite o primeiro valor: ") 
+//     n2 = prompt("Digite o segundo valor: ") 
+       
+//     var soma = parseInt(n1) + parseInt(n2)
+//     alert("O resultado da soma é: " + soma)
+// }
+
+// function subtrair(){
+//     var n1, n2
+//     n1 = prompt("Digite o primeiro valor: ") 
+//     n2 = prompt("Digite o segundo valor: ") 
+       
+//     var sub = parseInt(n1) - parseInt(n2)
+//     alert("O resultado da soma é: " + sub)
+// }
+// function multiplicar(){
+//     var n1, n2
+//     n1 = prompt("Digite o primeiro valor: ") 
+//     n2 = prompt("Digite o segundo valor: ") 
+       
+//     var multi = parseInt(n1) * parseInt(n2)
+//     alert("O resultado da soma é: " + multi)
+// }
+// function dividir(){
+//     var n1, n2
+//     n1 = prompt("Digite o primeiro valor: ") 
+//     n2 = prompt("Digite o segundo valor: ") 
+       
+//     var div = parseInt(n1) / parseInt(n2)
+//     alert("O resultado da soma é: " + div)
+// }
+
+// function clicou(){
+//     document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar<b>"
+// }
+
+// function redirecionar(){
+//     //outra janela
+//     window.open("https://www.youtube.com/?reload=9&gl=BR&hl=pt")
+//     //mesma janela
+//     window.location.href = "https://www.youtube.com/?reload=9&gl=BR&hl=pt"
+// }
+
+// function trocar(){
+//     alert("trocar texto")
+// } 
+
+// function trocar(){
+//     document.getElementById("mouseMove").innerHTML = "Obrigado por passar o mouse"
+// }
+// function voltar(){
+//     document.getElementById("mouseMove").innerHTML = "Passe o mouse aqui"
+// }
+
+// function mudarfoto2(elemento){
+//      document.getElementById("foto").src = "/img/foto2.jpeg"
+// }
+// function mudarfoto1(elemento){
+//      document.getElementById("foto").src = "/img/foto1.jpeg"
+// }
+// function mudarfoto2(elemento){
+//     elemento.src = "/img/foto2.jpeg"
+// }
+// function mudarfoto1(elemento){
+//     elemento.src = "/img/foto1.jpeg"
+// }
+
+// // function load(){
+// //     alert("Página carregada!")
+// // }
+
+// function change(elemento){
+//     alert(elemento.value)
+// }
